4bdb1a4b6eb69c9c6d1f7c37599e9b415c993c35
liuxing95 <1358974006>
2022-04-23 22:28:35 +0800

    feat: 2070-medium-drop-char

---
+++ 2070-medium-drop-char/index.ts
@@ -0,0 +1,50 @@
+/*
+  2070 - Drop Char
+  -------
+  by CaptainOfPhB (@CaptainOfPhB) #medium #template-literal #infer
+  
+  ### Question
+  
+  Drop a specified char from a string.
+  
+  For example:
+  
+  ```ts
+  type Butterfly = DropChar<' b u t t e r f l y ! ', ' '> // 'butterfly!'
+  ```
+  
+  > View on GitHub: https://tsch.js.org/2070
+*/
+
+
+/* _____________ Your Code Here _____________ */
+
+type DeleteChar<S extends string, C extends string> = S extends C ? '' : S
+
+type DropChar<S extends string, C extends string> = S extends `${infer First}${infer Rest}` ? `${DeleteChar<First, C>}${DropChar<Rest, C>}` : S
+
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type X = DropChar<'butter fly!', ''>
+type X1 = DropChar<'butter fly!', ' '>
+type cases = [
+  Expect<Equal<DropChar<'butter fly!', ''>, 'butterfly!'>>,
+  Expect<Equal<DropChar<'butter fly!', ' '>, 'butterfly!'>>,
+  Expect<Equal<DropChar<'butter fly!', '!'>, 'butter fly'>>,
+  Expect<Equal<DropChar<'    butter fly!        ', ' '>, 'butterfly!'>>,
+  Expect<Equal<DropChar<' b u t t e r f l y ! ', ' '>, 'butterfly!'>>,
+  Expect<Equal<DropChar<' b u t t e r f l y ! ', 'b'>, '  u t t e r f l y ! '>>,
+  Expect<Equal<DropChar<' b u t t e r f l y ! ', 't'>, ' b u   e r f l y ! '>>,
+]
+
+
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/2070/answer
+  > View solutions: https://tsch.js.org/2070/solutions
+  > More Challenges: https://tsch.js.org
+*/
+
