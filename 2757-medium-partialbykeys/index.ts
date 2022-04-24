8fa95309cbf7df48d4efdaf9019a9e7ff39710a2
liuxing95 <1358974006>
2022-04-24 22:40:35 +0800

    feat: 2757-medium-partialbykeys

---
+++ 2757-medium-partialbykeys/index.ts
@@ -0,0 +1,75 @@
+/*
+  2757 - PartialByKeys
+  -------
+  by jiangshan (@jiangshanmeta) #medium #object
+  
+  ### Question
+  
+  Implement a generic `PartialByKeys<T, K>` which takes two type argument `T` and `K`.
+  
+  `K` specify the set of properties of `T` that should set to be optional. When `K` is not provided, it should make all properties optional just like the normal `Partial<T>`.
+  
+  For example
+  
+  ```typescript
+  interface User {
+    name: string
+    age: number
+    address: string
+  }
+  
+  type UserPartialName = PartialByKeys<User, 'name'> // { name?:string; age:number; address:string }
+  ```
+  
+  > View on GitHub: https://tsch.js.org/2757
+*/
+
+
+/* _____________ Your Code Here _____________ */
+type MapObj<Obj extends Record<string, any>> = {
+  [Key in keyof Obj]: Obj[Key]
+}
+
+type PartialByKeys<T, K extends string = never> = [K] extends [never] ? Partial<T> : MapObj<Pick<T, Exclude<keyof T, K>> & {
+  [Key in K as Key extends keyof T ? Key : never]?: Key extends keyof T ? T[Key] : never
+}>
+
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+import { ExpectFalse, NotEqual } from '@type-challenges/utils'
+
+interface User {
+  name: string
+  age: number
+  address: string
+}
+
+interface UserPartialName {
+  name?: string
+  age: number
+  address: string
+}
+
+interface UserPartialNameAndAge {
+  name?: string
+  age?: number
+  address: string
+}
+
+type cases = [
+  Expect<Equal<PartialByKeys<User, 'name'>, UserPartialName>>,
+  Expect<Equal<PartialByKeys<User, 'name' | 'unknown'>, UserPartialName>>,
+  Expect<Equal<PartialByKeys<User, 'name' | 'age'>, UserPartialNameAndAge>>,
+  Expect<Equal<PartialByKeys<User>, Partial<User>>>,
+]
+
+
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/2757/answer
+  > View solutions: https://tsch.js.org/2757/solutions
+  > More Challenges: https://tsch.js.org
+*/
+
