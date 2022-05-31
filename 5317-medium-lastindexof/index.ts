e62e6e5eb880e13c06f7bcca05c8518e5eb4e68e
liuxing95 <1358974006>
2022-05-31 23:18:45 +0800

    feat: 5317-medium-lastindexof

---
+++ 5317-medium-lastindexof/index.ts
@@ -0,0 +1,53 @@
+/*
+  5317 - LastIndexOf
+  -------
+  by jiangshan (@jiangshanmeta) #medium #array
+  
+  ### Question
+  
+  Implement the type version of ```Array.lastIndexOf```, ```LastIndexOf<T, U>```  takes an Array ```T```, any ```U``` and returns the index of the last ```U``` in Array ```T```
+  
+  For example:
+  
+  ```typescript
+  type Res1 = LastIndexOf<[1, 2, 3, 2, 1], 2> // 3
+  type Res2 = LastIndexOf<[0, 0, 0], 2> // -1
+  ```
+  
+  > View on GitHub: https://tsch.js.org/5317
+*/
+
+
+/* _____________ Your Code Here _____________ */
+// 做题思路
+/**
+ * 倒着 进行过滤 最后 返回的 剩余数组的长度
+ */
+
+type LastIndexOf<T extends unknown[], U extends unknown> = T extends [...infer Head, infer Tail]
+  ? Equal<Tail, U> extends true
+    ? Head['length']
+    : LastIndexOf<Head, U>
+  : -1
+
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<LastIndexOf<[1, 2, 3, 2, 1], 2>, 3>>,
+  Expect<Equal<LastIndexOf<[2, 6, 3, 8, 4, 1, 7, 3, 9], 3>, 7>>,
+  Expect<Equal<LastIndexOf<[0, 0, 0], 2>, -1>>,
+  Expect<Equal<LastIndexOf<[string, 2, number, 'a', number, 1], number>, 4>>,
+  Expect<Equal<LastIndexOf<[string, any, 1, number, 'a', any, 1], any>, 5>>,
+]
+
+
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/5317/answer
+  > View solutions: https://tsch.js.org/5317/solutions
+  > More Challenges: https://tsch.js.org
+*/
+
